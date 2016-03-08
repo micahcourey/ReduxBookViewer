06e12b1eb62bfd1719a90b72556e9e1efb25dbe6
micahcourey <micahjcourey@gmail.com>
2016-03-07 19:26:08 -0800

    Add initial project files

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Switch: How To Change Things When Change Is Hard' },
+    { title: 'The Martian' },
+    { title: 'Eloquent JavaScript' },
+    { title: 'Programming Logic and Design' }
+  ]
+}
