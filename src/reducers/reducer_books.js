ad2d6dd5c4b6436ba8223d8c33f2ec75c30b202c
micahcourey <micahjcourey@gmail.com>
2016-03-11 22:49:29 -0800

    Add more sample data to reducer_books.js

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,7 @@ export default function() {
     { title: 'Switch: How To Change Things When Change Is Hard' },
     { title: 'The Martian' },
     { title: 'Eloquent JavaScript' },
-    { title: 'Programming Logic and Design' }
+    { title: 'Programming Logic and Design' },
+    { title: 'Lord of the Rings'}
   ]
 }
