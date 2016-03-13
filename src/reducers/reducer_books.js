ba60948937cb663713cfe68ca589f0decf1553e8
micahcourey <micahjcourey@gmail.com>
2016-03-12 16:40:46 -0800

    Add a pages property to the books and render a specific books details when clicked

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: 'Switch: How To Change Things When Change Is Hard' },
-    { title: 'The Martian' },
-    { title: 'Eloquent JavaScript' },
-    { title: 'Programming Logic and Design' },
-    { title: 'Lord of the Rings'}
+    { title: 'Switch: How To Change Things When Change Is Hard', pages: 101 },
+    { title: 'The Martian', pages: 234 },
+    { title: 'Eloquent JavaScript', pages: 134 },
+    { title: 'Programming Logic and Design', pages: 504 },
+    { title: 'Lord of the Rings', pages: 325 }
   ]
 }
