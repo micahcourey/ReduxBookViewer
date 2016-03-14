84b3a8b01abaacbc75924e9f9025381ba9836116
micahcourey <micahjcourey@gmail.com>
2016-03-13 19:55:39 -0700

    Add book data to book reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -4,6 +4,8 @@ export default function() {
     { title: 'The Martian', pages: 234 },
     { title: 'Eloquent JavaScript', pages: 134 },
     { title: 'Programming Logic and Design', pages: 504 },
-    { title: 'Lord of the Rings', pages: 325 }
+    { title: 'Lord of the Rings', pages: 325 },
+    { title: 'JavaScript For Cats', pages: 63 },
+    { title: 'JavaScript Garden', pages: 147 }
   ]
 }
